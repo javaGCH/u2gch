98905c3949808318a779eef17672ab038f9a94d8
javaGCH <794147680@qq.com>
2017-08-02 12:10:08 +0800

    Signed-off-by: javaGCH <794147680@qq.com>

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,18 @@
 
   public static void main (String[] args){
     system.out.println("Hello World!");
+
+  }
+
+public static void main (String[] args){
+    system.out.println("Hello World!");
+
+  }
+public static void main (String[] args){
+    system.out.println("Hello World!");
+
+  }
+public static void main (String[] args){
+    system.out.println("Hello World!");
+
   }
